2ca25c4140ce46cfaa104639f16e9fa56c9bfb2c
Andreas Velounias <>
2021-02-22 16:04:46 +0000

    Add provider package

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,26 +1,32 @@
+import 'package:app/providers/products_provider.dart';
 import 'package:app/screens/product_detail_screen.dart';
 import 'package:app/screens/products_overview_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'My Shop',
-      theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrange,
-          fontFamily: 'Lato'),
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (context) => ProductDetailScreen()
+    return ChangeNotifierProvider(
+      create: (context) {
+        return ProductsProvider();
       },
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'My Shop',
+        theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato'),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (context) => ProductDetailScreen()
+        },
+      ),
     );
   }
 }
---
+++ app/lib/widgets/products-grid.dart
@@ -0,0 +1,23 @@
+import 'package:app/providers/products_provider.dart';
+import 'package:app/widgets/product_item.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<ProductsProvider>(context);
+    final products = productsData.items;
+    return GridView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemCount: products.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 16.0,
+            mainAxisSpacing: 16.0),
+        itemBuilder: (context, index) => ProductItem(
+              product: products[index],
+            ));
+  }
+}
